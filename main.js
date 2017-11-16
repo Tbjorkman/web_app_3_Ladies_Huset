5af9e52c99624df5181bf05e4e90555f27975e94
Tbjorkman <tabithabjorkman@gmail.com>
2017-11-16 13:22:39 +0100

    latest version 15-11-17

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-/*
+
 function getAllEvents()
 {
     fetch("https://tabithabjorkman.com/wordpress/wp-json/wp/v2/events?_embed")
     .then(res=>res.json())
     .then(showEvents);
 }
- */
-
 
 function getCategories() {
     fetch("https://tabithabjorkman.com/wordpress/wp-json/wp/v2/categories?_embed")
@@ -35,11 +33,15 @@ function showCategories(categories) {
     categories.forEach(function(cat){
         if(cat.count > 0)
         {
-            let clone = catList.cloneNode(true);
-            let parent = document.querySelector('#categoryMenu');
-            clone.querySelector('a').textContent = cat.name;
-            clone.querySelector('a').setAttribute('href', 'index.html?cat_id=' + cat.id);
-            parent.appendChild(clone);
+            if(cat.id === 13 || cat.id ===  10 || cat.id === 7)
+            {
+                let clone = catList.cloneNode(true);
+                let parent = document.querySelector('#categoryMenu');
+                clone.querySelector('a').textContent = cat.name;
+                clone.querySelector('a').setAttribute('href', 'index.html?cat_id=' + cat.id);
+                parent.appendChild(clone);
+            }
+
         }
 
     });
@@ -53,42 +55,40 @@ function showSingleEvent(json) {
     document.querySelector("#single img").setAttribute("src", json._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
     document.querySelector('#single h1').textContent = json.title.rendered;
     document.querySelector('#single h6').textContent = json.acf.custom_category + ' - ' + json.acf.performing_arts;
-    document.querySelector('.date span').textContent = json.acf.event_date;
-    document.querySelector('.time span').textContent = json.acf.time;
-    document.querySelector('.content').innerHTML = json.content.rendered;
-    document.querySelector('.venue span').textContent = json.acf.venue;
+    document.querySelector('#single .date span').textContent = json.acf.event_date;
+    document.querySelector('#single .time span').textContent = json.acf.time;
+    document.querySelector('#single .content').innerHTML = json.content.rendered;
+    document.querySelector('#single .venue span').textContent = json.acf.venue;
     document.querySelector('#single .price span').textContent = json.acf.price;
 }
 
 function showEvents(e_data){
-    //console.log(e_data)
     let list = document.querySelector("#list");
     let template = document.querySelector("#eventTemplate").content;
 
     e_data.forEach(function(theEvent){
+        //console.log(theEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
 
-    let clone = template.cloneNode(true);
-    let title = clone.querySelector("h2");
-    let date = clone.querySelector("#date");
-    let time = clone.querySelector("#time");
-    let excerpt = clone.querySelector(".excerpt");
-    let price = clone.querySelector(".price span");
-    let img = clone.querySelector("img");
-    let link = clone.querySelector("a.readMore");
+        let clone = template.cloneNode(true);
+        let title = clone.querySelector("h2");
+        let date = clone.querySelector("#date");
+        let time = clone.querySelector("#time");
+        let excerpt = clone.querySelector(".excerpt");
+        let price = clone.querySelector(".price span");
+        let img = clone.querySelector("img");
+        let link = clone.querySelector("a.readMore");
 
-    //console.log(theEvent._embedded["wp:featuredmedia"][0].media_details.sizes);
-    img.setAttribute("src", theEvent._embedded["wp:featuredmedia"]
-        [0].media_details.sizes.medium.source_url);
-    title.textContent = theEvent.title.rendered;
-    date.textContent = theEvent.acf.event_date;
-    time.textContent = theEvent.acf.time;
-    excerpt.innerHTML = theEvent.excerpt.rendered;
-    price.textContent = theEvent.acf.price;
-    link.setAttribute("href", "event_page.html?id="+theEvent.id);
+        img.setAttribute("src", theEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
+        title.textContent = theEvent.title.rendered;
+        date.textContent = theEvent.acf.event_date;
+        time.textContent = theEvent.acf.time;
+        excerpt.innerHTML = theEvent.excerpt.rendered;
+        price.textContent = theEvent.acf.price;
+        link.setAttribute("href", "event_page.html?id="+theEvent.id);
 
-        console.log(clone);
+            //console.log(clone);
 
-    list.appendChild(clone);
+        list.appendChild(clone);
     });
 }
 
@@ -101,22 +101,26 @@ let catId = searchParams.get('cat_id');
 getCategories();
 
 
+
+getEventsByCategory(13);
+getEventsByCategory(10);
+getEventsByCategory(7);
+
 if(id)
 {
     getSingleEventById(id);
 }
-if(catId)
+/*
+else if(catId)
 {
     getEventsByCategory(catId);
 }
 else
 {
     //getAllEvents();
-    getEventsByCategory(13);
-    getEventsByCategory(10);
-    getEventsByCategory(7);
-}
 
+}
+*/
 
 let menu = document.querySelector('#off_canvas_menu');
 let caret = document.querySelector('#arrow_nav_header');
@@ -183,4 +187,6 @@ function rotateCarrot() {
     menu.classList.toggle('displayBlock');
     caret.classList.toggle('rotated');
 }
+
+
  */
